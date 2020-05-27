713a1bf7d00c643b9732dfb96d516bedf32a789e
freshLiver <freshliver.cys@gmail.com>
2020-05-27 20:27:06 +0800

    simple ui test

---
+++ Java_Project/java_pro/src/main/java/JP/java_pro/Ibase.java
@@ -0,0 +1,14 @@
+package JP.java_pro;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public interface Ibase {
+    public static void initBtn ( JButton btn , int width , int height , int x , int y ){
+        btn.setLocation ( x , y );
+        btn.setSize ( 100 , height );
+    }
+
+}
---
+++ Java_Project/java_pro/src/main/java/JP/java_pro/Main.java
@@ -0,0 +1,10 @@
+package JP.java_pro;
+
+public class Main {
+    public static void main ( String[] args ) {
+        // init UI 
+        UI mainUI = new UI ( "C_Chat Image", 400, 400 );
+
+        // 
+    }
+}
